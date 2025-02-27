85fbdaa246e6e5b5b7d24159b4f19a02a85040d7
Adrian506-coder <treviadrian18@gmail.com>
2025-02-27 12:53:12 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,198 @@
+# python.exe -m venv .venv
+# cd .venv/Scripts
+# activate.bat
+# py -m ensurepip --upgrade
+# pip install -r requirements.txt
+
+from flask import Flask
+
+from flask import render_template
+from flask import request
+from flask import jsonify, make_response
+
+import mysql.connector
+
+import datetime
+import pytz
+
+from flask_cors import CORS, cross_origin
+
+con = mysql.connector.connect(
+    host="185.232.14.52",
+    database="u760464709_23005038_bd",
+    user="u760464709_23005038_usr",
+    password="=1OcQV3fbJ"
+)
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def index():
+    if not con.is_connected():
+        con.reconnect()
+
+    con.close()
+
+    return render_template("index.html")
+
+@app.route("/app")
+def app2():
+    if not con.is_connected():
+        con.reconnect()
+
+    con.close()
+
+    return "<h5>Hola, soy la view app</h5>"
+
+@app.route("/productos")
+def productos():
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    SELECT idPostre,
+           nombrePostre,
+
+    FROM postres
+
+    LIMIT 10 OFFSET 0
+    """
+
+    cursor.execute(sql)
+    registros = cursor.fetchall()
+
+    # Si manejas fechas y horas
+    """
+    for registro in registros:
+        fecha_hora = registro["Fecha_Hora"]
+
+        registro["Fecha_Hora"] = fecha_hora.strftime("%Y-%m-%d %H:%M:%S")
+        registro["Fecha"]      = fecha_hora.strftime("%d/%m/%Y")
+        registro["Hora"]       = fecha_hora.strftime("%H:%M:%S")
+    """
+
+    return render_template("productos.html", productos=registros)
+
+@app.route("/productos/buscar", methods=["GET"])
+def buscarProductos():
+    if not con.is_connected():
+        con.reconnect()
+
+    args     = request.args
+    busqueda = args["busqueda"]
+    busqueda = f"%{busqueda}%"
+    
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    SELECT idProducto,
+           nombreProducto,
+    FROM postres
+
+    WHERE nombreProducto LIKE %s
+
+    ORDER BY idProducto DESC
+
+    LIMIT 10 OFFSET 0
+    """
+    val    = (busqueda, busqueda, busqueda)
+
+    try:
+        cursor.execute(sql, val)
+        registros = cursor.fetchall()
+
+        # Si manejas fechas y horas
+        """
+        for registro in registros:
+            fecha_hora = registro["Fecha_Hora"]
+
+            registro["Fecha_Hora"] = fecha_hora.strftime("%Y-%m-%d %H:%M:%S")
+            registro["Fecha"]      = fecha_hora.strftime("%d/%m/%Y")
+            registro["Hora"]       = fecha_hora.strftime("%H:%M:%S")
+        """
+
+    except mysql.connector.errors.ProgrammingError as error:
+        print(f"Ocurrió un error de programación en MySQL: {error}")
+        registros = []
+
+    finally:
+        con.close()
+
+    return make_response(jsonify(registros))
+
+@app.route("/producto", methods=["POST"])
+# Usar cuando solo se quiera usar CORS en rutas específicas
+# @cross_origin()
+def guardarProducto():
+    if not con.is_connected():
+        con.reconnect()
+
+    id          = request.form["idPostre"]
+    nombre      = request.form["nombrePostre"]
+    # fechahora   = datetime.datetime.now(pytz.timezone("America/Matamoros"))
+    
+    cursor = con.cursor()
+
+    if id:
+        sql = """
+        UPDATE postres
+
+        SET nombreProducto = %s,
+
+        WHERE idProducto = %s
+        """
+        val = (nombre, id)
+    else:
+        sql = """
+        INSERT INTO productos (Nombre_Producto, Precio, Existencias)
+                    VALUES    (%s,          %s,      %s)
+        """
+        val =                 (nombre)
+    
+    cursor.execute(sql, val)
+    con.commit()
+    con.close()
+
+    return make_response(jsonify({}))
+
+@app.route("/producto/<int:id>")
+def editarProducto(id):
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    SELECT idPostre, nombrePostre
+
+    FROM postres
+
+    WHERE idPostre = %s
+    """
+    val    = (id,)
+
+    cursor.execute(sql, val)
+    registros = cursor.fetchall()
+    con.close()
+
+    return make_response(jsonify(registros))
+
+@app.route("/producto/eliminar", methods=["POST"])
+def eliminarProducto():
+    if not con.is_connected():
+        con.reconnect()
+
+    id = request.form["id"]
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    DELETE FROM postres
+    WHERE idPostre = %s
+    """
+    val    = (id,)
+
+    cursor.execute(sql, val)
+    con.commit()
+    con.close()
+
+    return make_response(jsonify({}))
